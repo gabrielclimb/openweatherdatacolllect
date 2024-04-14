96e7fa1848e8c7b467c0a38fbe4d669294bfcf16
Gabriel Barbosa Soares <gabriel.soares@teya.com>
2024-04-14 19:31:45 +0100

    feat: docker-compose db and migrations

---
+++ migrations/0001_create_cities_table.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS cities (
+    id SERIAL PRIMARY KEY,
+    city_name VARCHAR(255) NOT NULL,
+    country_name VARCHAR(255) NOT NULL,
+    latitude DECIMAL(9, 7),
+    longitude DECIMAL(10, 7)
+);
+
+INSERT INTO
+    cities (city_name, country_name, latitude, longitude)
+VALUES
+    ('Sao Paulo', 'Brazil', -23.5475000, -46.6361122),
+    ('Lisbon', 'Portugal', 38.7166667, -9.1333332),
+    ('Porto', 'Portugal', 41.1500000, -8.6166668),
+    ('Zurich', 'Switzerland', 47.3666667, 8.5500002);
