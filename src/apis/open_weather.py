5057b8579d6fa7d1a39874536ac4afb5eb0bf613
Gabriel Barbosa Soares <gabriel.soares@teya.com>
2024-04-14 14:35:06 +0100

    chore: cities list

--- src/apis/open_weather.py
+++ src/apis/open_weather.py
@@ -1,7 +1,11 @@
+from typing import Optional
 from src.helpers.api_handler import APIBaseClass
 
 
 class OpenWeather(APIBaseClass):
+    base_url = "https://openweathermap.org"
+
     def re_auth(self): ...
 
-    def __init__(self, base_url: str): ...
+    def __init__(self, base_url: Optional[str] = None) -> None:
+        self.base_url = base_url if base_url else self.base_url
